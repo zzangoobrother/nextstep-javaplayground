3f5d6089760c3651c1998bdd55de76ba9d014baa
zzangoobrother <com1368@naver.com>
2022-02-03 22:45:38 +0900

    [refactor] 스트라이크 구현 리팩토링

--- src/main/java/com/example/nextstepjavaplayground/baseball/CompareNumber.java
+++ src/main/java/com/example/nextstepjavaplayground/baseball/CompareNumber.java
@@ -19,24 +19,36 @@ public class CompareNumber {
   }
 
   private void ballCount(String randomNum, String inputNum, int i, int j) {
-    if (numCompare(randomNum, inputNum, i, j)) {
+    if (ballNumCompare(randomNum, inputNum, i, j)) {
       ball++;
     }
   }
 
-  private boolean numCompare(String randomNum, String inputNum, int i, int j) {
+  private boolean ballNumCompare(String randomNum, String inputNum, int i, int j) {
     return i != j && randomNum.charAt(i) == inputNum.charAt(j);
   }
 
   public int strike(String randomNum, String inputNum) {
     strike = 0;
     for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        if (i == j && randomNum.charAt(i) == inputNum.charAt(j)) {
-          strike++;
-        }
-      }
+      strikeCompare(randomNum, inputNum, i);
     }
     return strike;
+  }
+
+  private void strikeCompare(String randomNum, String inputNum, int i) {
+    for (int j = 0; j < 3; j++) {
+      strikeCount(randomNum, inputNum, i, j);
+    }
+  }
+
+  private void strikeCount(String randomNum, String inputNum, int i, int j) {
+    if (strikeNumCompare(randomNum, inputNum, i, j)) {
+      strike++;
+    }
+  }
+
+  private boolean strikeNumCompare(String randomNum, String inputNum, int i, int j) {
+    return i == j && randomNum.charAt(i) == inputNum.charAt(j);
   }
 }
